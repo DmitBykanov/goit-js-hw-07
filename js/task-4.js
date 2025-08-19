573b407eb97e664eb0d5f7e5384ec1fd0107661d
dmitbykanov <dmitbykanov@gmail.com>
2025-08-19 13:45:26 +0300

    task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+const formSend= document.querySelector('.login-form');
+formSend.addEventListener('submit', onFormSubmit);
 
+function onFormSubmit(event) {
+  event.preventDefault();
+  const formData = new FormData(event.currentTarget);
+  const formValues = {};
+  formData.forEach((value, name) => {
+    formValues[name] = value.trim();
+  });
+
+  const email = formValues.email;
+  const password = formValues.password;
+
+  if (email === '' || password === '') {
+    return alert ('All form fields must be filled in');
+  }
+
+  console.log(formValues);
+  event.currentTarget.reset();
+}
